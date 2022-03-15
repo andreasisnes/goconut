b9e932b30de437853b94b61271035bcd33431476
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-03-15 16:02:11 +0100

    added extra checks trying to convert a type to the same type

--- cast.go
+++ cast.go
@@ -28,36 +28,84 @@ func CastAndTryAssignValue(from interface{}, to interface{}) interface{} {
 func castValue(to interface{}, from interface{}) interface{} {
 	switch to.(type) {
 	case string:
+		if res, ok := from.(string); ok {
+			return res
+		}
 		return cast.ToString(from)
 	case int:
+		if res, ok := from.(int); ok {
+			return res
+		}
 		return cast.ToInt(from)
 	case int64:
+		if res, ok := from.(int64); ok {
+			return res
+		}
 		return cast.ToInt64(from)
 	case int32:
+		if res, ok := from.(int32); ok {
+			return res
+		}
 		return cast.ToInt32(from)
 	case int16:
+		if res, ok := from.(int16); ok {
+			return res
+		}
 		return cast.ToInt16(from)
 	case int8:
+		if res, ok := from.(int8); ok {
+			return res
+		}
 		return cast.ToInt8(from)
 	case uint:
+		if res, ok := from.(uint); ok {
+			return res
+		}
 		return cast.ToUint(from)
 	case uint64:
+		if res, ok := from.(uint16); ok {
+			return res
+		}
 		return cast.ToUint16(from)
 	case uint32:
+		if res, ok := from.(uint32); ok {
+			return res
+		}
 		return cast.ToUint32(from)
 	case uint16:
+		if res, ok := from.(uint16); ok {
+			return res
+		}
 		return cast.ToUint16(from)
 	case uint8:
+		if res, ok := from.(uint8); ok {
+			return res
+		}
 		return cast.ToUint8(from)
 	case float64:
+		if res, ok := from.(float64); ok {
+			return res
+		}
 		return cast.ToFloat64(from)
 	case float32:
+		if res, ok := from.(float32); ok {
+			return res
+		}
 		return cast.ToFloat32(from)
 	case bool:
+		if res, ok := from.(bool); ok {
+			return res
+		}
 		return cast.ToBool(from)
 	case time.Time:
+		if res, ok := from.(time.Time); ok {
+			return res
+		}
 		return cast.ToTime(from)
 	case time.Duration:
+		if res, ok := from.(time.Duration); ok {
+			return res
+		}
 		return cast.ToDuration(from)
 	default:
 		return from
