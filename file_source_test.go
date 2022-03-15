28ccf3db6a291de465256fe9a149eca91f69a794
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-03-15 16:36:09 +0100

    initial testing

--- file_source_test.go
+++ file_source_test.go
@@ -10,17 +10,45 @@ import (
 
 var (
 	dataDir = "data"
+	yaml1   = path.Join(dataDir, "config1.yaml")
+	toml1   = path.Join(dataDir, "config1.toml")
 	json1   = path.Join(dataDir, "jsonconfig1.json")
 	json2   = path.Join(dataDir, "jsonconfig2.json")
 )
 
-func TestJsonFile(t *testing.T) {
-	builder := goconut.NewBuilder().
-		Add(NewFileProvider(json1, false, true)).
+func TestJsonObjectField(t *testing.T) {
+	config := goconut.NewBuilder().
 		Add(NewFileProvider(json1, false, true)).
 		Build()
 
-	var g string
-	builder.Get("SimpleField", &g)
-	assert.Equal(t, "<SimpleField>", g)
+	res := config.Get("SimpleField", nil)
+	assert.Equal(t, "<SimpleField-1>", res)
+}
+
+func TestJsonObjectFieldLayered(t *testing.T) {
+	config := goconut.NewBuilder().
+		Add(NewFileProvider(json1, false, true)).
+		Add(NewFileProvider(json2, false, true)).
+		Build()
+
+	res := config.Get("SimpleField", nil)
+	assert.Equal(t, "<SimpleField-2>", res)
+}
+
+func TestTomlObject(t *testing.T) {
+	config := goconut.NewBuilder().
+		Add(NewFileProvider(toml1, false, true)).
+		Build()
+
+	res := config.Get("SimpleField", nil)
+	assert.Equal(t, "<SimpleField-1>", res)
+}
+
+func TestYamlObject(t *testing.T) {
+	config := goconut.NewBuilder().
+		Add(NewFileProvider(yaml1, false, true)).
+		Build()
+
+	res := config.Get("SimpleField", nil)
+	assert.Equal(t, "<SimpleField-1>", res)
 }
