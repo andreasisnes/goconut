78cca4dc56ffededc5c05cf94ce3e25db6e666fd
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-03-16 12:32:52 +0100

    added more configuration properties

--- external.go
+++ external.go
@@ -1,5 +1,25 @@
 package goconut
 
+const (
+	RefreshAll RefreshPolicy = iota
+	RefreshCurrent
+	RefreshAllOver
+	RefreshAllUnder
+)
+
+type RefreshPolicy int
+
+type SourceOptions struct {
+	ReloadOnChange  bool
+	Optional        bool
+	SentinelOptions *SentinelOptions
+}
+
+type SentinelOptions struct {
+	Key           string
+	RefreshPolicy RefreshPolicy
+}
+
 type ISource interface {
 	Exists(key string) bool
 	Get(key string) interface{}
