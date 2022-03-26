853daaabb7e47e589b55d088009d2c226b7ead1f
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-03-26 17:16:14 +0100

    added sentinel tests

--- environment_variables_test.go
+++ environment_variables_test.go
@@ -24,7 +24,8 @@ func TestGet(t *testing.T) {
 	assert.Equal(t, expected, config.Get(key, nil))
 }
 
-func TestGetAfterAddedValue(t *testing.T) {
+func TestGetWithReloadOnChange(t *testing.T) {
+	t.Parallel()
 	config := newConfig(&EnvironmentVariablesOptions{
 		RefreshInterval: time.Second,
 		SourceOptions: goconut.SourceOptions{
@@ -32,12 +33,58 @@ func TestGetAfterAddedValue(t *testing.T) {
 		},
 	})
 
-	key := "TestGetAfterAddedValue"
+	key := "TestGetWithReloadOnChange"
 	expected := "TEST_VALUE"
 	os.Setenv(key, expected)
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second * 2)
 	result := config.Get(key, nil)
 
 	assert.Equal(t, expected, result)
+	config.Deconstruct()
+}
+
+func TestGetWithSentinel(t *testing.T) {
+	t.Parallel()
+	config := newConfig(&EnvironmentVariablesOptions{
+		RefreshInterval: time.Second,
+		SourceOptions: goconut.SourceOptions{
+			ReloadOnChange: false,
+			SentinelOptions: &goconut.SentinelOptions{
+				Key:           "TestGetWithSentinel",
+				RefreshPolicy: goconut.RefreshCurrent,
+			},
+		},
+	})
+
+	key := "TestGetWithSentinel"
+	expected := "TEST_VALUE"
+	os.Setenv(key, expected)
+	time.Sleep(time.Second * 2)
+	result := config.Get(key, nil)
+
+	assert.Equal(t, expected, result)
+	config.Deconstruct()
+}
+
+func TestGetNilWithSentinel(t *testing.T) {
+	t.Parallel()
+	config := newConfig(&EnvironmentVariablesOptions{
+		RefreshInterval: time.Second,
+		SourceOptions: goconut.SourceOptions{
+			ReloadOnChange: false,
+			SentinelOptions: &goconut.SentinelOptions{
+				Key:           "TestGetNilWithSentinelUnkownKey",
+				RefreshPolicy: goconut.RefreshCurrent,
+			},
+		},
+	})
+
+	key := "TestGetNilWithSentinel"
+	notExpected := "TEST_VALUE"
+	os.Setenv(key, notExpected)
+	time.Sleep(time.Second * 2)
+	result := config.Get(key, nil)
+
+	assert.Nil(t, result)
 	config.Deconstruct()
 }
